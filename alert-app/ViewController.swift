ff19e73d98c99d7a9e6f8302e265f93eee785179
Wallace Baldenebre <wallacecorin@gmail.com>
2021-12-05 20:01:24 -0300

    first commit

--- alert-app/ViewController.swift
+++ alert-app/ViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
-
+    var context = LAContext()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        context.localizedCancelTitle = "Enter Username/Password"
+        let reason = "Log in to your account"
+        context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason ) { success, error in
+            if success {
+                print("deu bom")
+            } else {
+                print(error?.localizedDescription ?? "Failed to authenticate")
+            }
+        }
     }
-
-
 }
-
